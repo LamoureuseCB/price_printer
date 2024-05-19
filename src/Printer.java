1aebe2fec4ccc87dfa2f9179570eb7ebaa09ba23
LamoureuseCB <148466239+LamoureuseCB@users.noreply.github.com>
2024-05-19 10:12:58 +0600

    Initial commit

---
+++ src/Printer.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+class Printer<T extends Number> {
+    private final List<T> list;
+
+    public Printer(List<T> list) {
+        this.list = list;
+    }
+
+    public void print() {
+        for (int i = 0; i < list.size(); i++) {
+            double price = list.get(i).doubleValue()/ 100;
+            System.out.println("Цена товара: " + price + " тг.");
+        }
+    }
+}
